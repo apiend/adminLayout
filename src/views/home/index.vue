1758683112d13923a407c5b12c000022a5875c8a
Diogoxiang <a233894432@gmail.com>
2018-11-30 13:49:40 +0800

    add project home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,90 @@
 <template>
-  <div class="hero is-fullheight">
-     el-container
-     {{now}}
+  <div class="container">
+    <!-- main header -->
+    <div class="main-header">
+      <el-row>
+        <el-col :span="12">
+          <nav class="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+              <li>
+                <a href="#">项目管理</a>
+              </li>
+            </ul>
+          </nav>
+        </el-col>
+
+        <el-col :span="8" :offset="4" class="text-right">
+          <el-button type="primary" icon="el-icon-news el-icon-right" size="medium">新建项目</el-button>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- main header end -->
+    <!-- main-filter -->
+    <div class="main-filter">
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入关键字搜索项目" v-model="searchTxt"></el-input>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary" icon="el-icon-search"></el-button>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- main-filter end -->
+    <div class="main-content">
+      <!-- main-content -->
+      <table class="table is-bordered is-striped is-fullwidth">
+        <thead>
+          <tr>
+            <th>序号</th>
+            <th>
+              项目名称
+            </th>
+            <th>
+               运行状态
+            </th>
+            <th>
+               操作状态
+            </th>
+            <th>
+              操作
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>16</th>
+            <td>38</td>
+            <td>11</td>
+            <td>9</td>
+            <td>
+               <el-button type="primary" size="mini">数据源管理</el-button>
+                <el-button type="success" size="mini">监控任务</el-button>
+                <el-button type="info" size="mini">编辑</el-button>
+                <el-button type="warning" size="mini">关闭</el-button>
+                <el-button type="danger" size="mini">开启</el-button>
+
+            </td>
+          </tr>
+          <tr>
+            <th>16</th>
+            <td>38</td>
+            <td>11</td>
+            <td>9</td>
+            <td>9</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- main - content -->
+    <div class="pageDiv">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000">
+      </el-pagination>
+    </div>
+    
   </div>
 </template>
 
@@ -14,7 +97,30 @@ define(["Vue"], function(Vue) {
     template: template,
     data: function() {
       return {
-        now: new Date()
+        now: new Date(),
+        searchTxt: "",
+        tableData: [
+          {
+            date: "2016-05-02",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1518 弄"
+          },
+          {
+            date: "2016-05-04",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1517 弄"
+          },
+          {
+            date: "2016-05-01",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄"
+          },
+          {
+            date: "2016-05-03",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1516 弄"
+          }
+        ]
       };
     },
     mounted: function() {
